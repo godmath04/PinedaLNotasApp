396c38effb64850222966c1cf80b4a08382fc9d0
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-15 15:13:16 -0500

    Se implementa la pagina principal de NotePage

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PinedaLNotasApp;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NotePage.xaml.cs
@@ -0,0 +1,25 @@
+namespace PinedaLNotasApp;
+
+public partial class NotePage : ContentPage
+{
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notas.txt");
+	public NotePage()
+	{
+		InitializeComponent();
+
+		if(File.Exists(_fileName))
+			txtEditor.Text = File.ReadAllText(_fileName);
+	}
+
+	private void btnGuardar(Object sender, EventArgs e) { 
+		File.WriteAllText(_fileName, txtEditor.Text);
+	}
+
+	private void btnBorrar(Object sender, EventArgs e) {
+		if(File.Exists(_fileName))
+			File.Delete(_fileName);
+
+		txtEditor.Text = string.Empty;
+
+	}
+}
