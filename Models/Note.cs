59384da3f5f0c41ed52d8dabc44114aea90c9648
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-25 11:09:30 -0500

    Actualizacion del trabajo de G1 al MVVM

--- Models/Note.cs
+++ Models/Note.cs
@@ -11,5 +11,57 @@ namespace PinedaLNotasApp.Models
         public string FileName { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
+
+        //Constructor
+        public Note()
+        {
+            FileName = $"{Path.GetRandomFileName()}.notas.txt";
+            Date = DateTime.Now;
+            Text = "";
+        }
+
+        //Metodos para el control, guardado o eliminacion
+        public void Save() =>
+            File.WriteAllText(System.IO.Path.Combine(FileSystem.AppDataDirectory, FileName), Text);
+
+        public void Delete() =>
+            File.Delete(System.IO.Path.Combine(FileSystem.AppDataDirectory, FileName));
+
+        //Cargar una nota por nombre de archivo
+        public static Note Load(string filename)
+        {
+            filename = System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+
+            if (!File.Exists(filename))
+                throw new FileNotFoundException("Unable to find file on local storage.", filename);
+
+            return
+                new()
+                {
+                    FileName = Path.GetFileName(filename),
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetLastWriteTime(filename)
+                };
+        }
+
+        //Enumerar notas del dispositivos y cargarlas a la clase 
+        public static IEnumerable<Note> LoadAll()
+        {
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            return Directory
+
+                    // Select the file names from the directory
+                    .EnumerateFiles(appDataPath, "*.notas.txt")
+
+                    // Each file name is used to load a note
+                    .Select(filename => Note.Load(Path.GetFileName(filename)))
+
+                    // With the final collection of notes, order them by date
+                    .OrderByDescending(note => note.Date);
+        }
+
     }
 }
